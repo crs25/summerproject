6540f0d8c20f6da89ca9e6b9cb4c1e6620c7f93b
Conor Straub <crs25@st-andrews.ac.uk>
2019-06-12 15:43:36 +0100

    Added greeting

--- hello.f90
+++ hello.f90
@@ -1,6 +1,13 @@
 
 program hello
+implicit none
+
+character(len=12) :: first_name
+
+first_name="Conor"
 
 print*,"Hey, handsome"
+
+print*,"Good afternoon",first_name,"how's it going?"
 
 end program
